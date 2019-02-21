59b954966cd07633f18ac22b4cc616ea1790d0fb
guilhermerecife <guilherme_liver@hotmail.com>
2019-02-21 20:55:14 -0300

    Message events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user conected!');
 
+    socket.emit('newMessage', {
+        from: 'mike@example.com',
+        text: 'Hey, what is going on?',
+        createdAt: 123
+    });
+
+    socket.on('createMessage', (newMessage) => {
+        console.log('Creating message', newMessage);
+    });
+
+    socket.on('createEmail', (newEmail) => {
+        console.log('Creating email', newEmail);
+    });
+
     socket.on('disconnect', () => {
         console.log('A user was dinconnected from server');
     });
